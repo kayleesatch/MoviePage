0b874cb743e63e37c676383fb10f2b3d1872ebb3
kayleesatch <kayleesatch@gmail.com>
2024-09-22 19:55:10 -0600

    working on drop-down menu

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 //API: "https://www.omdbapi.com/?i=tt3896198&apikey=b550a61&s=fast"
 function openMenu() {
-    document.body.classList += " menu--open"
+    document.body.classList += " open__menu"
 }
 
 function closeMenu() {
-    document.body.classList.remove('menu--open')
-}
-
-async function movieResults(event) {
-    const promise = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=b550a61&s=fast")
-    const result = await promise.json()
-    const movies = result.filter(element => element.fastMovies)
-    console.log(movieResults)
+    document.body.classList.remove('open__menu')
 }
