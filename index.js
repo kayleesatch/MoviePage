b54785a02f0467ba083493cca9d2deb8591c9bd6
kayleesatch <kayleesatch@gmail.com>
2024-10-17 19:04:40 -0600

    search bar works

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 //API: "https://www.omdbapi.com/?i=tt3896198&apikey=b550a61&s=fast"
 
-
-const movieEl = document.querySelector(".movie__results")
-
-async function movies(){
-    const movieList = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=b550a61&s=fast")
-    const movieData = await movieList.json()
-    movieEl.innerHTML = movieData.map((movieResults) => indexHTML(movieResults)).join("")
-}
-console.log(movieData)
-movies()
-//what is the difference between "key" and value?
-function showMovies(movieResults) {
-    localStorage.setItem("movie", movie)
-    window.location.href = `${window.location.origin}/index.html`
-}
-
-function MoviesHTML(movieResults) {
-    return `
+    const movieEl = document.querySelector(".movie__results")
+    const searchInput = document.querySelector(".search__input")
+    const searchButton = document.getElementById("search__btn")
+    
+    function indexHTML(movie) {
+        return `
         <div class="movie__card">
-            <img src="movie__poster">${movie.Poster}</img>
-            <h3 class="movie__title">${movie.Title}</h3>
-            <p class="movie__year">${movie.Year}</p>
+        <img src="${movie.Poster}" alt="${movie.Title} poster"/>
+        <h3 class="movie__title">${movie.Title}</h3>
+        <p class="movie__year">${movie.Year}</p>
         </div>`
-        
     }
-    //what am i supposed to be console.log-ing
-    console.log(movieEl)
+    
+    async function fetchMovies(query){
+
+        document.querySelector(".movies__loading").style.display = "block"
+
+        document.getElementById("results").style.display = "block"
+
+        try{
+            const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=b550a61&s=${query}`)
+            
+            if(!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+            
+            const movieData = await response.json()
+            document.querySelector(".movies__loading").style.display = "none"
+
+            if(movieData.Search) {
+                movieEl.innerHTML = movieData.Search.map((movieResults) => indexHTML(movieResults)).join("")
+            } else {
+                movieEl.innerHTML = "<p> No Movies Found.</p>"
+            }
+        } catch (error) {
+            console.error("Error fetching movies:", error)
+            document.querySelector(".movies__loading").style.display = "none"
+            movieEl.innerHTML = "<p>Error fetching movies. Please try again later.</p>"
+        }
+    }
+    
+    searchButton.addEventListener("click", () => {
+        const query = searchInput.value.trim()
+        if (query) {
+            fetchMovies(query)
+        }
+    })
+    
+    searchInput.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            searchButton.click()
+        }
+    })
+    
